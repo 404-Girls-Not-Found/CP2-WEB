794addff7d36992095cc86c8a21fedf1187a19e6
Estela <estela.mariano@icloud.com>
2026-05-12 22:09:47 -0300

    feat: Adicionando constante produtos no js

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+const produtos = [
+    {
+        nome: "Mulher Maravilha",
+        descricao: "Action figure da heroína Mulher Maravilha com traje original com escudo e lança.\n Altura: 30cm",
+        preco: 650,
+        imagem: "./src/assets/img/action-figure-wonder-woman.jpg"   
+    },
+
+    {
+        nome: "Bela e a Fera",
+        descricao: "Conjunto especial Studios Disney 100 anos com a Bela e a Fera em edição colecionável.\n Altura: 35cm",
+        preco: 2220.90,
+        imagem: "./src/assets/img/action-figure-bela-fera.jpg"
+    },
+
+    {
+        nome: "Pequena Sereia",
+        descricao: "Ariel em base temática com Linguado, Sebastião e outros elementos do filme.\n Altura: 25cm",
+        preco: 1560,
+        imagem: "./src/assets/img/action-figure-pequena-sereia.png"
+    },
+
+    {
+        nome: "Chewbacca e Wookie",
+        descricao: "Conjunto especial de Star Wars, Chewbacca e Han Solo juntos com base e fundo temático. \n Altura: 30cm",
+        preco: 1980,
+        imagem:"./src/assets/img/action-figure-chewbacca.png"
+    },
+
+    {
+        nome: "Aladdin e Jasmine",
+        descricao: "Alladin e Jasmine no tapete mágico com base do Palácio do Sultão.\n Altura: 40cm",
+        preco: 980.90,
+        imagem: "./src/assets/img/action-figure-alladin.png"
+    }
+];
